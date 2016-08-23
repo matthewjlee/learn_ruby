28c2043f257a2c2b0497e0c6b4937c222204bef4
Matthew Lee <matthewjlee256@gmail.com>
2016-08-22 23:58:43 -0400

    new edit

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,20 @@
 #write your code here
+def echo(command)
+	return command
+end
+
+def shout(command)
+	return command.upcase
+end
+
+def repeat(command)
+	return (command + " " + command)
+end
+
+def repeat(command, arg)
+	total_command = command
+	for iteration in 1..arg-1
+		total_command = total_command + " " + command 
+		return total_command
+	end
+end
